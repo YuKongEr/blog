aa47904b71e1ba14431c7ff15b17193d2045dadf
xp541130126 <541130126@qq.com>
2017-04-14 16:54:32 +0800

    完成ssm框架整合 完成博客类别dao的增删改查已经分页方法 完成后台管理界面框架 完成 博客类别分页显示

--- src/main/resources/db_blog.sql
+++ src/main/resources/db_blog.sql
@@ -15,13 +15,6 @@ CREATE TABLE `t_blogger` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `t_link` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '友情链接表id',
-  `linkname` VARCHAR(100) DEFAULT NULL COMMENT '友情链接名',
-  `linkurl` VARCHAR(200) DEFAULT NULL COMMENT '友情链接url',
-  `orderNum` INT(11) DEFAULT NULL COMMENT '链接排序',
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `t_blogtype` (
   `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '博客id',
@@ -30,31 +23,6 @@ CREATE TABLE `t_blogtype` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `t_blog` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '博客类型',
-  `title` VARCHAR(200) NOT NULL COMMENT '博客题目',
-  `summary` VARCHAR(400) DEFAULT NULL COMMENT '博客摘要',
-  `releaseDate` DATETIME DEFAULT NULL COMMENT '发布日期',
-  `clickHit` INT(11) DEFAULT NULL COMMENT '评论次数',
-  `replyHit` INT(11) DEFAULT NULL COMMENT '回复次数',
-  `content` TEXT COMMENT '博客内容',
-  `keyWord` VARCHAR(200) DEFAULT NULL COMMENT '关键字',
-  `type_id` INT(11) DEFAULT NULL COMMENT '外键关联博客类别',
-  PRIMARY KEY (`id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `t_blog_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `t_blogtype` (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `t_comment` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '评论表id',
-  `userIp` VARCHAR(50) DEFAULT NULL COMMENT '评论者的ip',
-  `content` VARCHAR(1000) DEFAULT NULL COMMENT '评论内容',
-  `commentDate` DATETIME DEFAULT NULL COMMENT '评论日期',
-  `state` INT(11) DEFAULT NULL COMMENT '审核状态',
-  `blog_id` INT(11) DEFAULT NULL COMMENT '外键关联具体博客',
-  PRIMARY KEY (`id`),
-  KEY `blog_id` (`blog_id`),
-  CONSTRAINT `t_comment_ibfk_1` FOREIGN KEY (`blog_id`) REFERENCES `t_blog` (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 
