621ae654e4601b58b374e5732348c8ce01ab516d
YuKongEr <541130126@qq.com>
2018-12-04 11:03:58 +0800

    :art:删除冗余文件

--- 1.sql
+++
@@ -1,8 +0,0 @@
-create table student_range(
-    sno number(4) constraint s_pk primary key,
-    sname varchar2(10) constraint s_uk unique,
-    sage number,
-    sex char(2),
-    cno number(2)
-    )
-    partition by range 
